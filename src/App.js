815d0e019d9f091c269b2e944ad69e5b64c0db52
zikunwang-terry <terryiochina@gmail.com>
2022-08-09 23:11:03 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import me from './me.jpg';
 import WindowContainer from './components/window-container';
 
 function App(children) {
@@ -6,6 +7,10 @@ function App(children) {
     <div className="App">
       <WindowContainer>
         <h1>Zikun Wang</h1>
+        <p>Hello, I am a Computer Science student in Boston University.</p>
+        <img className="my-photo" src={me} alt="Me"></img>
+        <p>LinkedIn: <a href="http://www.linkedin.com/in/zikunw">Zikun Wang</a></p>
+        <p>Github: <a href="https://github.com/zikunw">@zikunw</a></p>
       </WindowContainer>
     </div>
   );
